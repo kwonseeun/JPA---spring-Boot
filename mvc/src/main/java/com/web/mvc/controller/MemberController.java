5fc9d9e33784c317316fbbbe2e67fbe3967d4e20
kwonseeun <0115km@naver.com>
2022-03-22 17:45:01 +0900

    Member Controller 조회, 가입 로직 추가

--- mvc/src/main/java/com/web/mvc/controller/MemberController.java
+++ mvc/src/main/java/com/web/mvc/controller/MemberController.java
@@ -1,5 +1,46 @@
 package com.web.mvc.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.web.mvc.entity.MemberEntity;
+import com.web.mvc.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController // JSON 형태 결과값 반환 
+@RequiredArgsConstructor // final 객체를 Constructor Injection 해준다 
+@RequestMapping("/v1") // version1 API
 public class MemberController {
+	
+	private final MemberRepository memberRepository;
+	
+	/**
+	 * 멤버조회
+	 * @return
+	 * */
+	@GetMapping("member")
+	public List<MemberEntity> findAllMember(){
+		return memberRepository.findAll();
+	}
+	
+	/**
+	 * 회원가입 
+	 * @return
+	 * */
+	@PostMapping("member")
+	public MemberEntity signUp() {
+		final MemberEntity member = MemberEntity.builder()
+						.username("test_user@gmail.com")
+						.name("test user")
+						.build();
+		return memberRepository.save(member);
+	}
+	
+	
 
 }
